045ab172f0fe6f70a27f7924d9fc18c2da0fb81f
sylv <ryan@sylver.me>
2022-10-11 03:39:50 +0800

    feat: add toml loader

--- src/load-config.ts
+++ src/load-config.ts
@@ -1,12 +1,13 @@
 import { default as merge } from "merge";
+import { TOMLLoader } from "./index.js";
 import { ArgsLoader } from "./loaders/args.loader.js";
 import { EnvLoader } from "./loaders/env.loader.js";
 import { JSONLoader } from "./loaders/fs/json.loader.js";
 import { YAMLLoader } from "./loaders/fs/yaml.loader.js";
-import { resolveLoaders } from "./resolve-loaders.js";
 import type { LoaderResolvable } from "./resolve-loaders.js";
+import { resolveLoaders } from "./resolve-loaders.js";
 
-export const DEFAULT_LOADERS: LoaderResolvable[] = [JSONLoader, YAMLLoader, ArgsLoader, EnvLoader];
+export const DEFAULT_LOADERS: LoaderResolvable[] = [JSONLoader, YAMLLoader, TOMLLoader, ArgsLoader, EnvLoader];
 
 export interface LoadDataOptions {
   loaders?: LoaderResolvable[];
