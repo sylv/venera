8c082486d84799eb779d771ec13b68fd00c8ed56
sylv <ryan@sylver.me>
2022-10-16 23:50:43 +0800

    fix: import order causing undefined loaders

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+export * from "./loaders/index.js";
 export * from "./helpers/coerce-value.js";
 export * from "./helpers/constant-case-to-path.js";
 export * from "./load-config.js";
-export * from "./loaders/index.js";
 export * from "./resolve-loaders.js";
